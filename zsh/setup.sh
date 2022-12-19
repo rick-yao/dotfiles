cebfa7492ee54fa539e3f04d52e5a6a9f105d780
rick <abneryaoo@gmail.com>
2022-12-19 10:40:53 +0800

    update zsh setup.sh

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,5 +1,12 @@
-# install zsh and pure theme
+# install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-mkdir -p "$HOME/.zsh"
-git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+# link pure theme
+if [ -d "$HOME/.zsh/" ]; then
+	echo ".zsh文件夹存在"
+	ln -sf $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/pure "$HOME/.zsh/"
+else
+	echo ".zsh文件夹不存在"
+	mkdir "$HOME/.zsh" && ln -sf $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/pure "$HOME/.zsh/"
+fi
+#link zshrc
 ln -sf ./zshrc "$HOME/.zshrc"
