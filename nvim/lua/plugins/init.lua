d79b7b48a825593d371518206a70613d7b76d563
rick <abneryaoo@gmail.com>
2023-02-23 17:51:34 +0800

    基本加入lazy,未测试

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,41 @@
+return {
+  "williamboman/mason.nvim",
+  'sainnhe/everforest',
+  'rmagatti/alternate-toggler',
+  'windwp/nvim-autopairs',
+  'mg979/vim-visual-multi',
+  'gcmt/wildfire.vim',
+  'tpope/vim-surround',
+
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-cmdline',
+  'saadparwaiz1/cmp_luasnip',
+
+  'jose-elias-alvarez/null-ls.nvim',
+
+  'nvim-treesitter/nvim-treesitter',
+
+  'windwp/nvim-ts-autotag',
+
+  'j-hui/fidget.nvim',
+  {
+    "folke/noice.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    }
+  },
+  { 'kevinhwang91/nvim-ufo', dependencies = 'kevinhwang91/promise-async' },
+
+  -- commenting with gc
+  "numToStr/Comment.nvim",
+  "JoosepAlviste/nvim-ts-context-commentstring", -- work with comment to support tsx jsx and the like
+
+  -- "ggandor/leap.nvim",
+
+  -- git integration
+  "lewis6991/gitsigns.nvim",
+
+  "christoomey/vim-tmux-navigator", -- tmux & split window navigation)
+}
---
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,12 @@
+return {
+	-- "williamboman/mason-lspconfig.nvim",
+	-- config = function()
+	-- 	require("mason-lspconfig").setup({
+	-- 		ensure_installed = {
+	-- 			'lua_ls',
+	-- 			'tsserver',
+	-- 		},
+	-- 		automatic_installation = true,
+	-- 	})
+	-- end
+}
