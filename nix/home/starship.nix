de80a1853da9af92b181e6f874a07bfff8ee21f6
rick <abneryaoo@gmail.com>
2023-11-16 10:14:46 +0800

    update starship

--- nix/home/starship.nix
+++ nix/home/starship.nix
@@ -7,18 +7,64 @@
     enableNushellIntegration = true;
 
     settings = {
-      character = {
-        success_symbol = "[›](bold green)";
-        error_symbol = "[›](bold red)";
+      # character = {
+      #   success_symbol = "[›](bold green)";
+      #   error_symbol = "[›](bold red)";
+      # };
+      # aws = {
+      #   symbol = "🅰 ";
+      # };
+      # gcloud = {
+      #   # do not show the account/project's info
+      #   # to avoid the leak of sensitive information when sharing the terminal
+      #   format = "on [$symbol$active(\($region\))]($style) ";
+      #   symbol = "🅶 ️";
+      # };
+      format = lib.concatStrings [
+          "$username"
+          "$package"
+          "$hostname"
+          "$directory"
+          "$git_branch"
+          "$git_state"
+          "$git_status"
+          "$cmd_duration"
+          "$line_break"
+          "$python"
+          "$character"
+          ];
+      directory.style = "blue";
+      character ={
+        success_symbol = "[❯](purple)";
+        error_symbol = "[❯](red)";
+        vimcmd_symbol = "[❮](green)";
       };
-      aws = {
-        symbol = "🅰 ";
+      git_branch ={
+        format = "[$branch]($style)";
+        style = "bright-black";
       };
-      gcloud = {
-        # do not show the account/project's info
-        # to avoid the leak of sensitive information when sharing the terminal
-        format = "on [$symbol$active(\($region\))]($style) ";
-        symbol = "🅶 ️";
+      git_status ={
+        format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+        style = "cyan";
+        conflicted = "";
+        untracked = "";
+        modified = "";
+        staged = "";
+        renamed = "";
+        deleted = "";
+        stashed = "≡";
+      };
+      git_state = {
+        format = "'\([$state( $progress_current/$progress_total)]($style)\) '";
+        style = "bright-black";
+      };
+      cmd_duration = {
+        format = "[$duration]($style) ";
+        style = "yellow";
+      };
+      python ={
+        format = "[$virtualenv]($style) ";
+        style = "bright-black";
       };
     };
   };
