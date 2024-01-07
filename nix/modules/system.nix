5b45aab5d312979b4b46350112276d6da7274549
rick <abneryaoo@gmail.com>
2024-01-07 18:18:46 +0800

    test

--- nix/modules/system.nix
+++ nix/modules/system.nix
@@ -14,7 +14,7 @@
 
   system = {
     # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
-    activationScripts.postActivation.text = ''
+    activationScripts.postUserActivation.text = ''
       # activateSettings -u will reload the settings from the database and apply them to the current session,
       # so we do not need to logout and login again to make the changes take effect.
       echo "reload start..."
