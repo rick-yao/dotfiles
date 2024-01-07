e443a2aa215da9c32b49597af88c2a4eb43f71ad
rick <abneryaoo@gmail.com>
2024-01-07 18:17:57 +0800

    test

--- nix/modules/system.nix
+++ nix/modules/system.nix
@@ -17,6 +17,7 @@
     activationScripts.postActivation.text = ''
       # activateSettings -u will reload the settings from the database and apply them to the current session,
       # so we do not need to logout and login again to make the changes take effect.
+      echo "reload start..."
       /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
     '';
 
