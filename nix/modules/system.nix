41e5b7391365d30fe06bedcce6c66ed6f69ac767
rick <abneryaoo@gmail.com>
2024-01-10 21:04:48 +0800

    enable system pref

--- nix/modules/system.nix
+++ nix/modules/system.nix
@@ -81,42 +81,42 @@
       #
       # All custom entries can be found by running `defaults read` command.
       # or `defaults read xxx` to read a specific domain.
-      # CustomUserPreferences = {
-      #   ".GlobalPreferences" = {
-      #     # automatically switch to a new space when switching to the application
-      #     AppleSpacesSwitchOnActivate = true;
-      #   };
-      #   NSGlobalDomain = {
-      #     # Add a context menu item for showing the Web Inspector in web views
-      #     WebKitDeveloperExtras = true;
-      #   };
-      #   "com.apple.finder" = {
-      #     ShowExternalHardDrivesOnDesktop = true;
-      #     ShowHardDrivesOnDesktop = true;
-      #     ShowMountedServersOnDesktop = true;
-      #     ShowRemovableMediaOnDesktop = true;
-      #     _FXSortFoldersFirst = true;
-      #     # When performing a search, search the current folder by default
-      #     FXDefaultSearchScope = "SCcf";
-      #   };
-      #   "com.apple.desktopservices" = {
-      #     # Avoid creating .DS_Store files on network or USB volumes
-      #     DSDontWriteNetworkStores = true;
-      #     DSDontWriteUSBStores = true;
-      #   };
-      #   "com.apple.screensaver" = {
-      #     # Require password immediately after sleep or screen saver begins
-      #     askForPassword = 1;
-      #     askForPasswordDelay = 0;
-      #   };
-      #   "com.apple.screencapture" = {
-      #     location = "~/Desktop";
-      #     type = "png";
-      #   };
-      #   "com.apple.AdLib" = { allowApplePersonalizedAdvertising = false; };
-      #   # Prevent Photos from opening automatically when devices are plugged in
-      #   "com.apple.ImageCapture".disableHotPlug = true;
-      # };
+      CustomUserPreferences = {
+        # ".GlobalPreferences" = {
+        #   # automatically switch to a new space when switching to the application
+        #   AppleSpacesSwitchOnActivate = true;
+        # };
+        NSGlobalDomain = {
+          # Add a context menu item for showing the Web Inspector in web views
+          WebKitDeveloperExtras = true;
+        };
+        "com.apple.finder" = {
+          ShowExternalHardDrivesOnDesktop = true;
+          ShowHardDrivesOnDesktop = true;
+          ShowMountedServersOnDesktop = true;
+          ShowRemovableMediaOnDesktop = true;
+          _FXSortFoldersFirst = true;
+          # When performing a search, search the current folder by default
+          FXDefaultSearchScope = "SCcf";
+        };
+        "com.apple.desktopservices" = {
+          # Avoid creating .DS_Store files on network or USB volumes
+          DSDontWriteNetworkStores = true;
+          DSDontWriteUSBStores = true;
+        };
+        "com.apple.screensaver" = {
+          # Require password immediately after sleep or screen saver begins
+          askForPassword = 1;
+          askForPasswordDelay = 0;
+        };
+        # "com.apple.screencapture" = {
+        #   location = "~/Desktop";
+        #   type = "png";
+        # };
+        "com.apple.AdLib" = { allowApplePersonalizedAdvertising = false; };
+        # Prevent Photos from opening automatically when devices are plugged in
+        # "com.apple.ImageCapture".disableHotPlug = true;
+      };
 
       # loginwindow = {
       #   GuestEnabled = false; # disable guest user
