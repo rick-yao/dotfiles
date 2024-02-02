ba4280d7fb1ed9326e8830baaec0b5e47593a876
rick <abneryaoo@gmail.com>
2024-02-02 13:46:49 +0800

    test: linux

---
+++ flake.nix
@@ -0,0 +1,103 @@
+{
+  description = "Nix for macOS configuration";
+  
+  outputs = inputs @{self, nixpkgs, darwin, home-manager, ...}: let 
+   
+    overlays = [
+     inputs.rust-overlay.overlays.default
+    ];
+    
+    mkSystem = import ./lib/mksystem.nix {
+      inherit overlays nixpkgs inputs;
+    };
+    in {
+    
+    darwinConfigurations."Ricks-MacBook-Air"  = mkSystem "Ricks-MacBook-Air" {
+      system = "aarch64-darwin";
+      user   = "rick";
+      darwin = true;
+    };
+    
+    homeConfigurations.${username} = mkSystem "pve-build" {
+      system = "x86_64-linux";
+      user   = "rick";
+      linux  = true;
+    };
+    
+    formatter.${system} = nixpkgs.legacyPackages.${system}.alejandra;
+    
+    };
+    
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    darwin,
+    home-manager,
+    rust-overlay,
+    ...
+  }: {
+    # NOTE: please update the whole "hostname" placeholder string to your own hostname!
+    # such as darwinConfigurations.mymac = darwin.lib.darwinSystem {
+    darwinConfigurations."Ricks-MacBook-Air" = darwin.lib.darwinSystem {
+      system = "aarch64-darwin"; # change this to "aarch64-darwin" if you are using Apple Silicon
+      modules = [
+        ./darwin/nix-core.nix
+        ./darwin/system.nix
+        ./darwin/apps.nix
+        ./darwin/host-users.nix
+
+        # home manager
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.extraSpecialArgs = inputs;
+
+          # NOTE: replace "yourusername" with your own username!
+          home-manager.users.rick = import ./home.nix;
+        }
+
+        ({pkgs, ...}: {
+          nixpkgs.overlays = [rust-overlay.overlays.default];
+          environment.systemPackages = [
+            (pkgs.rust-bin.stable.latest.default.override {
+              extensions = ["rust-src"];
+            })
+          ];
+        })
+      ];
+    };
+
+    # nix codee formmater
+    formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.alejandra;
+  };
+  
+  nixConfig = {
+    experimental-features = ["nix-command" "flakes"];
+
+    substituters = [
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://cache.nixos.org"
+    ];
+    
+    auto-optimise-store = true;
+  };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # home-manager, used for managing user configuration
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+}
---
+++ lib/mksystem.nix
@@ -0,0 +1,66 @@
+# This function creates a NixOS system based on our VM setup for a
+# particular architecture.
+{ nixpkgs, overlays, inputs }:
+
+name:
+{
+  system,
+  user,
+  darwin ? false,
+  linux ? false,
+}:
+
+let
+  pkgs = nixpkgs.legacyPackages.${system};
+  # The config files for this system.
+  machineConfig = ../machines/${name}.nix;
+  userOSConfig = ../users/${user}/${if darwin then "darwin" else "linux" }.nix;
+  userHMConfig = ../users/${user}/home-manager.nix;
+
+  # NixOS vs nix-darwin functionst
+  systemFunc = if darwin then inputs.darwin.lib.darwinSystem else inputs.home-manager.lib.homeManagerConfiguration;
+  home-manager = if darwin then inputs.home-manager.darwinModules else {};
+in systemFunc if darwin then {
+  inherit system;
+
+  modules = [
+    # Apply our overlays. Overlays are keyed by system type so we have
+    # to go through and apply our system type. We do this first so
+    # the overlays are available globally.
+    { nixpkgs.overlays = overlays; }
+
+    machineConfig
+    userOSConfig
+    home-manager.home-manager {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.users.${user} = import userHMConfig {
+        isWSL = isWSL;
+        inputs = inputs;
+      };
+    } 
+
+    extraSpecialArgs = {inherit rust-overlay};
+    # We expose some extra arguments so that our modules can parameterize
+    # better based on these values.
+    {
+      config._module.args = {
+        currentSystem = system;
+        currentSystemName = name;
+        currentSystemUser = user;
+        isWSL = isWSL;
+        inputs = inputs;
+      };
+    }
+  ];
+} else {
+    
+      inherit pkgs;
+
+      modules = [
+        ./home.nix
+        # ./overlay/rust.nix
+      ];
+
+      # extraSpecialArgs = {inherit rust-overlay;};
+}
