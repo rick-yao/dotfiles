4b0022b40b148561cdb0313dce566d61e0e9c557
rick <abneryaoo@gmail.com>
2024-02-18 04:21:41 +0000

    feat: disable sign git in linux

--- home-manager/linux/git.nix
+++ home-manager/linux/git.nix
@@ -1,12 +1,12 @@
 {...}: {
   programs.git = {
-    extraConfig = {
-      gpg.ssh.program = "/home/rick/.nix-profile/bin/gpg2";
-    };
+    # extraConfig = {
+      # gpg.ssh.program = "/home/rick/.nix-profile/bin/gpg2";
+    # };
 
-    signing = {
-      key = "30AA9065ECFE9D2E";
-      signByDefault = true;
-    };
+    # signing = {
+      # key = "30AA9065ECFE9D2E";
+      # signByDefault = true;
+    # };
   };
 }
