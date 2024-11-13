7c8ede06da0dbc63ece4937b92d862488e555622
Rick Yao <abneryaoo@gmail.com>
2024-11-13 15:03:42 +0800

    fix: build err in mac

--- home-manager/public/pkgs.nix
+++ home-manager/public/pkgs.nix
@@ -2,7 +2,6 @@
   home.packages = with pkgs; [
     xh
     htop
-    ncdu
     procs
     lazydocker
     fastfetch
--- machines/darwin/nix-core.nix
+++ machines/darwin/nix-core.nix
@@ -29,5 +29,5 @@
 
   # Manual optimise storage: nix-store --optimise
   # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
-  nix.settings.auto-optimise-store = true;
+  nix.optimise.automatic = true;
 }
