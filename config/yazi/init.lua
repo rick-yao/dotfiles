928714ca154a1ee5f05ef1bb12dff7c32b473cfe
Rick Yao <abneryaoo@gmail.com>
2024-12-17 08:24:10 +0800

    feat: update yazi git theme

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -1,2 +1,13 @@
-require("starship"):setup()
+THEME.git = THEME.git or {}
+THEME.git.modified = ui.Style():fg("blue")
+THEME.git.deleted = ui.Style():fg("red"):bold()
+
+THEME.git.modified_sign = "M"
+THEME.git.deleted_sign = "D"
+THEME.git.ignored_sign = "I"
+THEME.git.added_sign = "A"
+THEME.git.updated_sign = "U"
+THEME.git.untracked_sign = "??"
 require("git"):setup()
+
+require("starship"):setup()
