5b5942cfb8c7a7a2fb9849cd591e8fa33b1b28d0
Rick Yao <abneryaoo@gmail.com>
2025-04-06 11:43:38 +0800

    fix: remove masapps due to macos 15.4

--- machines/darwin/apps.nix
+++ machines/darwin/apps.nix
@@ -31,23 +31,23 @@
     # You need to install all these Apps manually first so that your apple account have records for them.
     # otherwise Apple Store will refuse to install them.
     # For details, see https://github.com/mas-cli/mas
-    masApps = {
-      # NOTE: use https://github.com/mas-cli/mas to check the app id.
-      Wechat = 836500024;
-      TailScale = 1475387142;
-      Mindnode = 1289197285;
-      # CommandX = 2141473041;
-      Amphetamine = 937984704;
-      QQ = 451108668;
-      Telegram = 747648890;
-      Copyless = 993841014;
-      RDP = 1295203466;
-      Xcode-Cleaner = 1296084683;
-      Tick-Tick = 966085870;
-      Run-Cat = 1429033973;
-      Server-Cat = 1501532023;
-      # Wecom = 1189898970;
-    };
+    # masApps = {
+    #   # NOTE: use https://github.com/mas-cli/mas to check the app id.
+    #   Wechat = 836500024;
+    #   TailScale = 1475387142;
+    #   Mindnode = 1289197285;
+    #   # CommandX = 2141473041;
+    #   Amphetamine = 937984704;
+    #   QQ = 451108668;
+    #   Telegram = 747648890;
+    #   Copyless = 993841014;
+    #   RDP = 1295203466;
+    #   Xcode-Cleaner = 1296084683;
+    #   Tick-Tick = 966085870;
+    #   Run-Cat = 1429033973;
+    #   Server-Cat = 1501532023;
+    #   # Wecom = 1189898970;
+    # };
 
     taps = [
       "homebrew/services"
