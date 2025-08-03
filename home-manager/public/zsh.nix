14f01ffa79f97f0a5deb4a9308dbd28c4f3b341a
Rick Yao <abneryaoo@gmail.com>
2025-08-03 14:28:32 +0800

    fix: dotDir

--- home-manager/public/zsh.nix
+++ home-manager/public/zsh.nix
@@ -5,7 +5,7 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     historySubstringSearch.enable = true;
-    dotDir = ".config/zsh-nix";
+    dotDir = "\${config.home.homeDirectory}/.config/zsh-nix";
     history = {
       path = "$HOME/.config/zsh-nix/.zsh_history";
       save = 10000;
