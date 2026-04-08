51d4aef3f2ea008d5770b0672e1538bab32f2f8d
Rick Yao <abneryaoo@gmail.com>
2026-04-08 19:22:55 +0800

    fix: remove mas

--- machines/darwin/apps.nix
+++ machines/darwin/apps.nix
@@ -31,21 +31,21 @@
     # You need to install all these Apps manually first so that your apple account have records for them.
     # otherwise Apple Store will refuse to install them.
     # For details, see https://github.com/mas-cli/mas
-    masApps = {
-      Wechat = 836500024;
-      Mindnode = 1289197285;
-      CommandX = 6448461551;
-      Amphetamine = 937984704;
-      # QQ = 451108668;
-      Telegram = 747648890;
-      Copyless = 993841014;
-      # RDP = 1295203466;
-      Xcode-Cleaner = 1296084683;
-      Tick-Tick = 966085870;
-      Run-Cat = 1429033973;
-      Server-Cat = 1501532023;
-      # Wecom = 1189898970;
-    };
+    # masApps = {
+    #   Wechat = 836500024;
+    #   Mindnode = 1289197285;
+    #   CommandX = 6448461551;
+    #   Amphetamine = 937984704;
+    #   # QQ = 451108668;
+    #   Telegram = 747648890;
+    #   Copyless = 993841014;
+    #   # RDP = 1295203466;
+    #   Xcode-Cleaner = 1296084683;
+    #   Tick-Tick = 966085870;
+    #   Run-Cat = 1429033973;
+    #   Server-Cat = 1501532023;
+    #   # Wecom = 1189898970;
+    # };
 
     taps = [
       "1password/tap"
