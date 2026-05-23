2422d905a1ff69d4187012ad4e42fe18ffa2a164
Rick Yao <abneryaoo@gmail.com>
2026-05-23 04:49:57 +0000

    feat: add git sign plugin

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -1,13 +1,3 @@
-th.git = th.git or {}
-th.git.modified = ui.Style():fg("blue")
-th.git.deleted = ui.Style():fg("red"):bold()
-
-th.git.modified_sign = "M"
-th.git.deleted_sign = "D"
-th.git.ignored_sign = "I"
-th.git.added_sign = "A"
-th.git.updated_sign = "U"
-th.git.untracked_sign = "??"
 require("git"):setup({
 	-- Order of status signs showing in the linemode
 	order = 1500,
