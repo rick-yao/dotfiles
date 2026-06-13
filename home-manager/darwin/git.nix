5054b8bc4a1db003669a9dd231f97fc6e1c873cf
Rick Yao <abneryaoo@gmail.com>
2026-06-13 16:39:49 +0800

    fix: remove ssh agent

--- home-manager/darwin/git.nix
+++ home-manager/darwin/git.nix
@@ -1,12 +1,11 @@
 {...}: {
   programs.git = {
     settings = {
-      gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
       gpg.format = "ssh";
     };
-
+    
     signing = {
-      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDcijLLEuNPXo3ETb+euCqmvzBlgFOmA092igevTSICB";
+      key = "~/.ssh/id_ed25519.pub";
       signByDefault = true;
     };
   };
